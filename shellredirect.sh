6464281e65311f240e47c75a7230dfcf49d58f02
nxb4951 <47089538+nxb4951@users.noreply.github.com>
2021-09-28 11:18:35 -0400

    Finished functionality

--- shellredirect.sh
+++ shellredirect.sh
@@ -13,7 +13,7 @@ echo Users able to log in:
 cat /etc/passwd | grep -v /usr/sbin/nologin | grep -v /bin/false
 if [$username != ""]; then
 	echo $username
-	start=$(cat /etc/passwd | grep $username:)
-	echo $start
 	# TODO actually edit the /etc/passwd file and replace line with one ending in /bin/false
+	usermod -L $username
+	usermod -s /bin/false $username
 fi
