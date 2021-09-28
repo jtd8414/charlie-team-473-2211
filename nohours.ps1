7e0df41a674bdc2bc27eaf5826af11bc278a890c
neb1239 <47090373+neb1239@users.noreply.github.com>
2021-09-28 13:15:51 -0400

    Add files via upload
    Pats script for changing user hours  AD

---
+++ nohours.ps1
@@ -0,0 +1,8 @@
+﻿# AD No hours script
+
+[byte[]]$NoHours = 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+
+Get-ADuser -Filter * | Out-Gridview -PassThru -Title "Select Users to Disable" | Foreach-Object { 
+    $_ | Disable-ADAccount
+    $_ | Set-ADUser -Replace @{logonhours = $NoHours}
+}
