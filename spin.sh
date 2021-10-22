d96d8faa1eaa2f6696fb65498e2cfeac236aa078
jsk8927 <89865603+jsk8927@users.noreply.github.com>
2021-10-22 12:08:15 -0400

    Update spin.sh
    increased sleep time between rotations

--- spin.sh
+++ spin.sh
@@ -3,11 +3,11 @@
 while [ true ] # runs forever
 do
 	xrandr -o left # rotates the display so left is up
-	sleep 1 # wait for 1 seconds
+	sleep 5 # wait for 1 seconds
 	xrandr -o inverted # rotates the display so its upside down
-	sleep 1 # wait for 1 seconds
+	sleep 5 # wait for 1 seconds
 	xrandr -o right # rotates the display so right is up
-	sleep 1 # wait for 1 seconds
+	sleep 5 # wait for 1 seconds
 	xrandr -o normal # rotates the display so its normal
-	sleep 1 # wait for 1 seconds
+	sleep 5 # wait for 1 seconds
 done
